835e442dd80f07be3a08a9e1bc4ab49ee451a250
Steven Liu <xl2948@columbia.edu>
2020-02-28 17:30:21 -0500

    Mostly finished part1

--- part1/mylist.c
+++ part1/mylist.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
-#include "mylist.c"
+#include <stdio.h>
+#include "mylist.h"
 
 struct Node *addFront(struct List *list, void *data){
 	if(data == 0){
@@ -9,14 +10,17 @@ struct Node *addFront(struct List *list, void *data){
     front->data = data;
     front->next = list->head;
     list->head = front;
+	//printf("%d \n", front);
+	//printf("%d \n", front->next);
+    //printf("%f \n", *(double *)(front->data));
 	return front;
 }
 
 void traverseList(struct List *list, void (*f)(void *)){
-	struct Node *pointer = list;
-	while (pointer -> next != 0){
-		(*f)(pointer);
-		pointer++;
+	struct Node *pointer = list->head;
+	while (pointer != 0){
+		(*f)(pointer->data);
+		pointer = pointer->next;
 	}
 }
 
@@ -31,20 +35,24 @@ int compareDouble(const void *data1, const void *data2){
 }
 
 struct Node *findNode(struct List *list, const void *dataSought, int (*compar)(const void *, const void *)){
-	struct Node *pointer = list;
-    while (pointer -> next != 0){
+	struct Node *pointer = list->head;
+    while (pointer != 0){
+		//printf("1 ");
         if(!compar(pointer->data, dataSought)){
 			return pointer;
 		}
-		pointer++;
+		pointer = pointer->next;
     }
 	return NULL;
 }
 
 void *popFront(struct List *list){
+	if(list->head == 0){
+		return 0;
+	}
 	struct Node *pointer = list->head;
 	void *data = pointer->data;
-	*list->head = *list->head->next;
+	list->head = list->head->next;
 	free(pointer);
 	return data;
 }
@@ -58,9 +66,10 @@ void removeAllNodes(struct List *list){
 }
 
 struct Node *addAfter(struct List *list, struct Node *prevNode, void *data){
-	if(!prevNode)
-		addFront(list, data0);
-	    struct Node *add = (struct Node *)malloc(sizeof(struct Node));
+	if(!prevNode){
+		return addFront(list, data);
+	}
+	struct Node *add = (struct Node *)malloc(sizeof(struct Node));
     add->next = prevNode->next;
     add->data = data;
     prevNode->next = add;
@@ -68,11 +77,16 @@ struct Node *addAfter(struct List *list, struct Node *prevNode, void *data){
 }
 
 void reverseList(struct List *list){
-	  struct Node *prv = NULL;
-      struct Node *cur = list->head;
-      struct Node *nxt;
+	struct Node *prv = NULL;
+    struct Node *cur = list->head;
+    struct Node *nxt;
 
-      while (cur) {
-		
-	  }
+    while (cur) {
+		nxt = cur->next;
+		cur->next = prv;
+		prv = cur;
+		cur = nxt;
+		//printf("1 ");
+	}
+	list->head = prv;
 }
